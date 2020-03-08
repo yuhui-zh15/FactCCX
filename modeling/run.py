59de3ec53497014854482e6b14b3d4b59569cfc1
Yuhui Zhang <zyh.thu@gmail.com>
2020-03-08 11:23:56 -0700

    supress logging

--- modeling/run.py
+++ modeling/run.py
@@ -438,7 +438,7 @@ def main():
     # Setup logging
     logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                         datefmt = '%m/%d/%Y %H:%M:%S',
-                        level = logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+                        level = logging.ERROR if args.local_rank in [-1, 0] else logging.WARN)
     logger.warning("Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
                     args.local_rank, device, args.n_gpu, bool(args.local_rank != -1), args.fp16)
 
